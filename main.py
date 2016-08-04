207f95e815dcca67a85d917348bf8f03f29e7d2e
JanBaunooo <janbaunooo@gmail.com>
2016-08-04 17:47:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -205,8 +205,8 @@ def main():
     captureImage = False
     # Events Loop
     while mainLoopRunning:
-        # Limit FPS to 60fps
-        # clock.tick(60)
+        Limit FPS to 30fps
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 mainLoopRunning = False
